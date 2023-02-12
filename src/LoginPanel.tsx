cedaf1e12b03c4b0ddc26c858f0fc14711e12d37
Jannchie <jannchie@gmail.com>
2023-02-12 15:32:17 +0900

    fix: env

--- src/LoginPanel.tsx
+++ src/LoginPanel.tsx
@@ -3,7 +3,7 @@ import { Btn, Panel } from 'roku-ui';
 export function LoginPanel() {
   return (
     <Panel padding>
-      <Btn onClick={() => { window.location.href = 'http://127.0.0.1:8080/auth/github'; }}>
+      <Btn onClick={() => { window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/github`; }}>
         Login By Github
       </Btn>
     </Panel>
