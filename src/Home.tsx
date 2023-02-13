8ab890a29644fdec09eb9a859f87f4a2b8807a18
Jannchie <jannchie@gmail.com>
2023-02-13 17:09:04 +0900

    feat: home

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,36 @@
-import { Container } from 'roku-ui';
-import { LoginPanel } from './LoginPanel';
+import { Btn, Container } from 'roku-ui';
 export function Home() {
   return (
-    <Container style={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '1rem',
-      padding: '1rem',
-    }}>
-      <LoginPanel />
-    </Container>
+    <>
+      <div style={{ margin: '0rem 20rem', textAlign: 'center' }}>
+        <div style={{
+          fontSize: '7rem',
+          fontWeight: 'bold',
+          margin: '6rem 0rem 0rem 0rem',
+        }}>
+          <span>
+          Code.
+          </span>
+          <span>
+            Time
+          </span>
+        </div>
+        <div className='text-frontground-3'>
+          CodeTime is a productivity tool for software developers. It helps developers track the amount of time they spend on coding projects, and provides insights into their coding habits and productivity.
+        </div>
+        <Btn size="lg" border style={{marginTop: '1rem'}} onClick={() => { window.location.href = `${import.meta.env.VITE_API_BASE_URL}/auth/github`; }}>
+          Login By Github
+        </Btn>
+      </div>
+
+      <Container style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        padding: '1rem',
+      }}>
+        {/* <LoginPanel /> */}
+      </Container>
+    </>
   );
 }
